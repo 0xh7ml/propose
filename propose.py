e78cfc8436c576f4d7125ab7bb8739b9de8a39be
Md Saikat <42938253+D4rk-h7ml@users.noreply.github.com>
2019-05-13 01:29:44 +0600

    Add files via upload

---
+++ propose.py
@@ -0,0 +1,57 @@
+import sys , time
+def hp(s):
+  for c in s + '\n':
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(5./100)
+hp(" Hey pritty girl can you tell me ure name  ?? ")
+nae =input(" plz input ure name here:")
+hp("Roses are red.....")
+hp("Sky is Blue.......")
+hp("Why I'm always.....")
+hp("Thinking about u..... ^_^ ")
+hp("Why this sweet feelings I've feeled can u tell me plz??")
+hp("Because ,,, Because ,,,, Because ,,,,")
+hp('''\033[31;1m
+                               _____ 
+                              |_   _|
+                                | |  
+                                | |  
+                               _| |_ 
+                              |_____|
+''')
+hp('''\033[31;1m
+                    z$6*#""""*c     :@ $$****$$$$L
+                  .@$F          "N..$F         '*$$
+                 /$F             '$P             '$$r
+                d$"                                #$      '%C"""$
+               4$F                                  $k    ud@$ JP
+               M$                                   J$*Cz*#" Md"
+               MR                              'dCum#$       "
+               MR                                    $
+               4$                                   4$
+                $L   ==========L o v e=========     MF
+                '$                                 4$
+                 ?B .z@r                           $
+               .+(2d"" ?                          $~
+    +$c  .z4Cn*"   "$.                           $
+'#*M3$Eb*""         '$c                         $
+   /$$RR              #b                      .R
+   6*"                 ^$L                   JF
+                         "$                 $
+                           "b             u"
+                             "N.        xF
+                               '*c    zF
+                                  "N@"
+
+
+''')
+hp('''\033[31;1m
+                                __     __       
+                                \ \   / /         
+                                 \ \_/ /__  _   _ 
+                                  \   / _ \| | | |
+                                   | | (_) | |_| |
+                                   |_|\___/ \__,_|''')
+hp('\n')
+hp("Hey,"+ nae +" ""Will u be my own ?? ^_^ ")
